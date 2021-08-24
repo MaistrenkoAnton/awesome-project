0eb8ab6904e058a8badf8c9eef91b0763f4b6d75
Anton <it2015maistrenko@gmail.com>
2021-08-24 21:16:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
+const http = require('http')
+const server = http.createServer((req, res) => {
+    console.log('New connection');
+    res.end('Hello Awesome');
 
+});
+const PORT = process.PORT || 8080;
+server.listen(PORT, () => console.log('Listening'));
